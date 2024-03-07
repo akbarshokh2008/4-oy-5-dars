61753e2684386fb5f63e7446d5c199773f655ab7
akbarshokh2008 <mosinovakbarjon9@gmail.com>
2024-03-07 19:43:08 +0500

    ...

---
+++ js/app.js
@@ -0,0 +1,70 @@
+// 17 - MASALA
+// let achko_2 = +prompt("ikki achkolik gollarni kiriting");
+// let achko_3 = +prompt("uch achkolik gollarni kiriting");
+
+// function jamiGollar(ochko2, ochko3) {
+//   let result = `${ochko2 * 2} + ${ochko3 * 3} = ${ochko2 * 2 + ochko3 * 3} `;
+//   return result;
+// }
+// console.log(jamiGollar(achko_2, achko_3));
+
+// 18 - MASALA
+// let matn = prompt("Hohlagan matn kiriting");
+
+// function nameString(n) {
+//   let result = n + "Edabet";
+//   return result;
+// }
+// console.log(nameString(matn));
+
+// 19 - MASALA
+// let son_1 = +prompt("birinchi sonni kiriting");
+// let son_2 = +prompt("ikkinchi sonni kiriting");
+
+// function jamiGollar(a, b) {
+//   if (a + b < 100) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// jamiGollar(son_1, son_2);
+
+// 20 - MASALA
+// function sonQoshish(n) {
+//   let son = [];
+
+//   for (let i = 1; i <= n; i++) {
+//     son.push(i);
+//   }
+//   return son;
+// }
+// console.log(sonQoshish(+prompt("son kiriting")));
+
+// 21 - MASALA
+
+// let tovuq = prompt("tovuqni nechtaligini kiriting");
+// let qoy = prompt("qoyni nechtaligini kiriting");
+// let mol = prompt("molni nechtaligini kiriting");
+
+// function jamiOyoqlar(a, b, c) {
+//   let result = `${a * 2} + ${b * 4} + ${c * 4} = ${a * 2 + b * 4 + c * 4} `;
+//   return result;
+// }
+// console.log(jamiOyoqlar(tovuq, qoy, mol));
+
+// FIZZBUZZ
+// function FIZZBUZZ(n) {
+//   for (let i = 1; i <= n; i += 1) {
+//     if (i % 5 == 0 && i % 3 == 0) {
+//       console.log("FIZZBUZZ");
+//     } else if (i % 5 == 0) {
+//       console.log("FIZZ");
+//     } else if (i % 3 == 0) {
+//       console.log("BUZZ");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
+// FIZZBUZZ(16);
